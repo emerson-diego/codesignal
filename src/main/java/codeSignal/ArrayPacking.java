5a6c4b74493be1ab6d60642f6c0a6eaf4fb4a436
Emerson Diego <diegowebby@gmail.com>
2020-04-14 19:34:50 -0300

    arrayPacking

---
+++ src/main/java/codeSignal/ArrayPacking.java
@@ -0,0 +1,21 @@
+package codeSignal;
+
+public class ArrayPacking {
+
+    public static void main(String[] args) {
+        int[] a = { 24, 85, 0 };
+        int result = arrayPacking(a);
+        System.out.println(result);
+    }
+
+    static int arrayPacking(int[] a) {
+
+        String R = "";
+        for (int i : a) {
+            R = String.format("%8s", Integer.toBinaryString(i)).replace(' ', '0') + R;
+        }
+        return Integer.parseInt(R, 2);
+
+    }
+
+}
